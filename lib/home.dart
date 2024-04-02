db02f58632f1ac9b622c87b208a77d8616ca555b
Chloe Gao <58396550+chgaobt@users.noreply.github.com>
2024-04-01 22:58:43 -0400

    tried to fix background issue

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 
 class home extends StatefulWidget {
-  const home({super.key});
+  const home({Key? key}) : super(key: key);
 
   @override
   State<home> createState() => _homeState();
@@ -13,40 +15,52 @@ class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-          SizedBox(
-            height: MediaQuery.of(context).size.height/1.2,
-            width: MediaQuery.of(context).size.width,
-            child: Image.asset(
-              'images/homeScreen.png',
-              fit: BoxFit.fill,
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            height: 80,
-            width: 160,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 144, 169, 85),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25.0),
-              child: Text(
-                "Add/Take a Photo",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    'images/homeScreen.png',
+                    fit: BoxFit.fill,
+                  ),
                 ),
-              ),
-            )
+                const Positioned(
+                  top: 350,
+                  left: 170,
+                  child: Text(
+                  "Select/Take a Photo",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),
+                ),
+                ),
+                
+                  
+                // ElevatedButton(
+                //   onPressed: () async {
+                //   }, 
+                //   child: Row(
+                //     mainAxisSize: MainAxisSize.min,
+                //     children: [
+                //       Icon(Icons.camera_alt_outlined, size: 100),
+                //     ]
+                //   )
+                // ),
+              ],
+              
+      
+            ),
           ),
-        ],
-      )
+        ),
+      ),
     );
   }
 }
