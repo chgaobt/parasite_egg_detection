4f53df56a2ab8f3d134e876b89a4c1ee6b265fa9
Chloe Gao <58396550+chgaobt@users.noreply.github.com>
2024-04-24 00:17:11 -0400

    POST and GET request with python Flask

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,14 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:parasite_egg_detection/request.dart';
+import 'package:dio/dio.dart';
 
 class home extends StatefulWidget {
   const home({Key? key}) : super(key: key);
@@ -37,7 +39,7 @@ class _homeState extends State<home> {
               // height: 200,
               // width: 150,
               child: Text(
-                'Take/Select a Photo',
+                'Select a Photo',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Colors.black,
@@ -55,18 +57,33 @@ class _homeState extends State<home> {
                     ),
                     ElevatedButton(
                       onPressed: () async {
-                        //final PickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
-                        var data = await getData('http://127.0.0.1:5000/');
-                        var decodedData = jsonDecode(data);
-                        print(decodedData['query']);
+                        final ImagePicker _picker = ImagePicker();
+                        final PickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+                        File file = File(PickedFile!.path); 
+                        var request = http.MultipartRequest('POST', Uri.parse('http://127.0.0.1:5000/'));
+                        request.files.add(await http.MultipartFile.fromPath('imagefile', file.path));
+
+
+                        try {
+                          var res = await request.send();
+                          if(res.statusCode == 200) {
+                            var data = await getData('http://127.0.0.1:5000/');
+                            var decodedData = jsonDecode(data);
+                            print(decodedData['query']);
+                            print('done');
+                          }
+                          else {
+                            print('failed to send');
+                          }
+                        }
+                        catch (e) {
+                          print('Error sending photo: ${e}');
+                        }                        
+                        //var data = await res.stream.bytesToString();
+                    
+                        print('sent photo');
                         //showAlertDialog(context, decodedData['query']);
                         //print(decodedData['query'][0]);
-                        // if(PickedFile != null){
-                        //   setState(() {
-                        //     image = XFile(PickedFile.path);
-                        //   });
-                        //   print(PickedFile.path);
-                        // }
                       },
                       child: Row(
                         mainAxisSize: MainAxisSize.min,
@@ -85,30 +102,3 @@ class _homeState extends State<home> {
   }
 }
 
-// showAlertDialog(BuildContext context, String name) {
-
-//     // set up the button
-//     Widget okayButton = FlatButton(
-//       child: Text("Okay!"),
-//       onPressed: () {
-//         Navigator.pop(context);
-//       },
-//     );
-
-//     // set up the AlertDialog
-//     AlertDialog alert = AlertDialog(
-//       title: Text("Flower"),
-//       content: Text("The name of flower is: $name"),
-//       actions: [
-//         okayButton,
-//       ],
-//     );
-
-//     // show the dialog
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return alert;
-//       },
-//     );
-//   }
